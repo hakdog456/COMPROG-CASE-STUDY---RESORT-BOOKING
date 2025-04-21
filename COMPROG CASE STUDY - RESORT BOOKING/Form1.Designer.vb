ba3c42698f43d11dfeed116080f8bb832074c05a
hakdog456 <dustingualberto7@gmail.com>
2025-04-21 16:20:13 +0800

    Ngiyorks

--- COMPROG CASE STUDY - RESORT BOOKING/Form1.Designer.vb
+++ COMPROG CASE STUDY - RESORT BOOKING/Form1.Designer.vb
@@ -40,7 +40,7 @@ Partial Class Form1
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(171, 231)
+        Me.Label2.Location = New System.Drawing.Point(175, 180)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(432, 32)
         Me.Label2.TabIndex = 1
--- COMPROG CASE STUDY - RESORT BOOKING/Form1.vb
+++ COMPROG CASE STUDY - RESORT BOOKING/Form1.vb
@@ -1,3 +1,3 @@
 ﻿Public Class Form1
-  #rororooror - elies456
+
 End Class
