90ebb1a404d8d035cf4e5bdb837110df5ae11517
hakdog456 <dustingualberto7@gmail.com>
2025-04-21 15:39:23 +0800

    Initial commit to new sample branch

--- COMPROG CASE STUDY - RESORT BOOKING/Form1.Designer.vb
+++ COMPROG CASE STUDY - RESORT BOOKING/Form1.Designer.vb
@@ -22,10 +22,44 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.title = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'title
+        '
+        Me.title.AutoSize = True
+        Me.title.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.title.Location = New System.Drawing.Point(254, 59)
+        Me.title.Name = "title"
+        Me.title.Size = New System.Drawing.Size(222, 32)
+        Me.title.TabIndex = 0
+        Me.title.Text = "NOMS RESORT"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(268, 207)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(208, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Sample Branch"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.title)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents title As Label
+    Friend WithEvents Label1 As Label
 End Class
